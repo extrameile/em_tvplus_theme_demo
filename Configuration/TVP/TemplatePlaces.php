e9c3f2663e71ef10f1bf8d0bbd325dcf1b4058f1
Alexander Opitz <opitz.alexander@googlemail.com>
2020-04-05 19:24:06 +0200

    [TASK] Make compatible with latest state of buchholz branch

--- Configuration/TVP/TemplatePlaces.php
+++ Configuration/TVP/TemplatePlaces.php
@@ -3,17 +3,17 @@ declare(strict_types = 1);
 
 return [
     '283274d1-5281-4939-8dd4-e1e8c987d275' => [
-        'name' => 'Simple Pages',
-        'path' => 'EXT:em_tvplus_theme_simple/Resources/Private/Templates/Pages',
-        'renderer' => \Ppi\TemplaVoilaPlus\Renderer\XpathRenderer::NAME,
+        'name' => 'Demo Pages',
+        'path' => 'EXT:em_tvplus_theme_demo/Resources/Private/Templates/Pages',
         'recursive' => true, // @TODO Not yet implemented
         'scope' => \Ppi\TemplaVoilaPlus\Domain\Model\AbstractDataStructure::SCOPE_PAGE,
+        'handler' => \Ppi\TemplaVoilaPlus\Handler\Place\TemplateYamlPlaceHandler::NAME,
     ],
     '49da302f-e70c-4836-a40b-67c32d1b089e' => [
-        'name' => 'Simple FCEs',
-        'path' => 'EXT:em_tvplus_theme_simple/Resources/Private/Templates/Fces',
-        'renderer' => \Ppi\TemplaVoilaPlus\Renderer\XpathRenderer::NAME,
+        'name' => 'Demo FCEs',
+        'path' => 'EXT:em_tvplus_theme_demo/Resources/Private/Templates/Fces',
         'recursive' => true, // @TODO Not yet implemented
         'scope' => \Ppi\TemplaVoilaPlus\Domain\Model\AbstractDataStructure::SCOPE_FCE,
+        'handler' => \Ppi\TemplaVoilaPlus\Handler\Place\TemplateYamlPlaceHandler::NAME,
     ],
 ];
--- ext_tables.php
+++ ext_tables.php
@@ -2,4 +2,4 @@
 defined('TYPO3_MODE') or die();
 
 // @TODO This line can be removed after cache is implemented
-\Ppi\TemplaVoilaPlus\Utility\ExtensionUtility::registerExtension('em_tvplus_theme_simple');
+\Ppi\TemplaVoilaPlus\Utility\ExtensionUtility::registerExtension('em_tvplus_theme_demo');

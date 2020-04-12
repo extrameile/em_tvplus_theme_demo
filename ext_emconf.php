242940a88df7015b10b7da3fff1b11e8b9ed1454
Alexander Opitz <opitz.alexander@googlemail.com>
2020-04-13 01:23:35 +0200

    [TASK] Release 0.1.0 depending on TV+ 8.0.0-alpha.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,12 +5,12 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Opitz',
     'author_email' => 'opitz@extrameile-gehen.de',
     'author_company' => 'Extrameile GmbH',
-    'version' => '0.0.1',
+    'version' => '0.1.0',
     'state' => 'alpha',
     'clearcacheonload' => 1,
     'constraints' => [
         'depends' => [
-            'templavoilaplus' => '8.0.0-8.99.99',
+            'templavoilaplus' => '8.0.0-alpha.1',
         ],
     ],
 ];

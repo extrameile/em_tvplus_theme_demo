32095bd7f523f243ecb30223d6eb2edf0d079b18
Alexander Opitz <opitz.alexander@googlemail.com>
2020-04-26 17:14:25 +0200

    [TASK] Fix naming of directory

--- Configuration/TVP/DataStructurePlaces.php
+++ Configuration/TVP/DataStructurePlaces.php
@@ -4,13 +4,13 @@ declare(strict_types = 1);
 return [
     'EM\TVP\Theme\Demo\Page\DataStructure' => [
         'name' => 'Demo Pages',
-        'path' => 'EXT:em_tvplus_theme_demo/Resources/Private/DataSources/Pages',
+        'path' => 'EXT:em_tvplus_theme_demo/Resources/Private/DataStructure/Pages',
         'scope' => \Ppi\TemplaVoilaPlus\Domain\Model\Scope::SCOPE_PAGE,
         'loadSaveHandler' => \Ppi\TemplaVoilaPlus\Handler\LoadSave\XmlLoadSaveHandler::$identifier,
     ],
     'EM\TVP\Theme\Demo\Fce\DataStructure' => [
         'name' => 'Demo FCEs',
-        'path' => 'EXT:em_tvplus_theme_demo/Resources/Private/DataSources/Fces',
+        'path' => 'EXT:em_tvplus_theme_demo/Resources/Private/DataStructure/Fces',
         'scope' => \Ppi\TemplaVoilaPlus\Domain\Model\Scope::SCOPE_FCE,
         'loadSaveHandler' => \Ppi\TemplaVoilaPlus\Handler\LoadSave\XmlLoadSaveHandler::$identifier,
     ],

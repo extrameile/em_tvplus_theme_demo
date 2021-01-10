5339e72183a36246976f75edb39cab5dbffe64e9
Alexander Opitz <opitz@extrameile.agency>
2021-01-10 16:40:03 +0100

    Add configuration to demonstrate moving of NewContentElementWizard elements

--- Configuration/TVP/NewContentElementWizard.php
+++ Configuration/TVP/NewContentElementWizard.php
@@ -6,9 +6,18 @@ return [
         'common' => [
             // after/before see TYPO3\CMS\Core\Service\DependencyOrderingService
             'after' => ['fce'],
+            'move' => [
+                'common_header' => ['tab' => 'interactive', 'before' => 'interactive_accordion']
+            ],
         ],
         'data' => [
             'unset' => true,
+        ],
+        'fce' => [
+            'move' => [
+                'fce_EM\TVP\Theme\Demo\Fce\Mapping:Card.tvp.yaml' => ['tab' => 'common', 'before' => ['common_bullets', 'common_image']],
+                'fce_EM\TVP\Theme\Demo\Fce\Mapping:TextboxWithLink.tvp.yaml' => ['tab' => 'common', 'before' => ['fce_EM\TVP\Theme\Demo\Fce\Mapping:Card.tvp.yaml']],
+            ],
         ],
     ],
     'simpleView' => [

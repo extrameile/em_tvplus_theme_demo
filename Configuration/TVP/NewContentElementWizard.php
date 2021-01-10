ee035c98afebe5f362d060cdb5f2736985913d6b
Alexander Opitz <opitz@extrameile.agency>
2021-01-10 16:00:06 +0100

    Add configuration to demonstrate before/after of NewContentElementWizard

--- Configuration/TVP/NewContentElementWizard.php
+++ Configuration/TVP/NewContentElementWizard.php
@@ -4,6 +4,10 @@ declare(strict_types = 1);
 return [
     'overwrites' => [
         'common' => [
+            // after/before see TYPO3\CMS\Core\Service\DependencyOrderingService
+            'after' => ['fce'],
+        ],
+        'data' => [
             'unset' => true,
         ],
     ],

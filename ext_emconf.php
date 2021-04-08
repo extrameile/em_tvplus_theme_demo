8b3678b441d49ce24b8f8869f181f4d6b78b18d1
Alexander Opitz <opitz@extrameile.agency>
2021-04-08 12:28:46 +0200

    Depend on alpha 6

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,12 +5,12 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Opitz',
     'author_email' => 'opitz@extrameile-gehen.de',
     'author_company' => 'Extrameile GmbH',
-    'version' => '0.4.0',
+    'version' => '0.4.1',
     'state' => 'alpha',
     'clearcacheonload' => 1,
     'constraints' => [
         'depends' => [
-            'templavoilaplus' => '8.0.0-alpha.5',
+            'templavoilaplus' => '8.0.0-alpha.6',
         ],
     ],
 ];

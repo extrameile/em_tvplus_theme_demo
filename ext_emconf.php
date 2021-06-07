81a0cc7c9024c1eafa608c56e3b9a8636e78c7c1
Alexander Opitz <opitz@extrameile.agency>
2021-06-07 18:27:01 +0200

    [RELEASE] Release 0.4.2 (depending on alpha7)

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,12 +5,12 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Opitz',
     'author_email' => 'opitz@extrameile-gehen.de',
     'author_company' => 'Extrameile GmbH',
-    'version' => '0.4.1',
+    'version' => '0.4.2',
     'state' => 'alpha',
     'clearcacheonload' => 1,
     'constraints' => [
         'depends' => [
-            'templavoilaplus' => '8.0.0-alpha.6',
+            'templavoilaplus' => '8.0.0-alpha.7',
         ],
     ],
 ];

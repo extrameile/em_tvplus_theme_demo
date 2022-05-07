174cd4fa4b76ab9eb41ad8d4eb3cee5f37e132b6
Alexander Opitz <opitz@extrameile.agency>
2022-05-07 12:16:18 +0200

    [RELEASE] Releasing 0.5.0
    * Depending on TV+ 8.0.3
    * Adding bootstrap carousel

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,12 +5,12 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Opitz',
     'author_email' => 'opitz@extrameile-gehen.de',
     'author_company' => 'Extrameile GmbH',
-    'version' => '0.4.3',
+    'version' => '0.5.0',
     'state' => 'alpha',
     'clearcacheonload' => 1,
     'constraints' => [
         'depends' => [
-            'templavoilaplus' => '8.0.0-alpha.7',
+            'templavoilaplus' => '8.0.3',
         ],
     ],
 ];

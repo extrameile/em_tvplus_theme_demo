b163d4558b54d232801740e1d51c66cb20635c77
Alexander Opitz <opitz@extrameile.agency>
2022-05-16 22:11:53 +0200

    [FEATURE] Add feature indentation for DataStructure saving

--- Configuration/TVP/DataStructurePlaces.php
+++ Configuration/TVP/DataStructurePlaces.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 return [
     'EM\TVP\Theme\Demo\Page\DataStructure' => [
@@ -7,11 +7,13 @@ return [
         'path' => 'EXT:em_tvplus_theme_demo/Resources/Private/DataStructure/Pages',
         'scope' => \Tvp\TemplaVoilaPlus\Domain\Model\Scope::SCOPE_PAGE,
         'loadSaveHandler' => \Tvp\TemplaVoilaPlus\Handler\LoadSave\XmlLoadSaveHandler::$identifier,
+        'indentation' => 4,
     ],
     'EM\TVP\Theme\Demo\Fce\DataStructure' => [
         'name' => 'Demo FCEs',
         'path' => 'EXT:em_tvplus_theme_demo/Resources/Private/DataStructure/Fces',
         'scope' => \Tvp\TemplaVoilaPlus\Domain\Model\Scope::SCOPE_FCE,
         'loadSaveHandler' => \Tvp\TemplaVoilaPlus\Handler\LoadSave\XmlLoadSaveHandler::$identifier,
+        'indentation' => 4,
     ],
 ];
